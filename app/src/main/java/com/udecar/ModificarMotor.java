f33a53331f2d21366b4b73732e334324906ed8a7
Norelly Estrada <ynestrada@ucundinamarca.edu.co>
2021-04-11 19:35:53 -0500

    prueba

--- app/src/main/java/com/udecar/ModificarMotor.java
+++ app/src/main/java/com/udecar/ModificarMotor.java
@@ -11,4 +11,10 @@ public class ModificarMotor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modificar_motor);
     }
+
+
+
+
+
+
 }
